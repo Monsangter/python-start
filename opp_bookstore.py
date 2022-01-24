6f654a9452c447a0f26a42bb2f9f9874b2af9c62
monsangter <sangdobae@icloud.com>
2022-01-24 21:50:28 +0900

    아아

--- opp_bookstore.py
+++ opp_bookstore.py
@@ -10,5 +10,13 @@ class Book:
         print('가격 : ', self.price)
         print('저자 : ', self.author)
 
-    def __init__(self):
+    def __init__(self, title, price, author):
+        self.setData(title, price, author)
         print('책 객체를 새로 만들었어요~')
+
+
+b=Book()
+b.setData('누가 내 치즈를 먹었을까', '300원', '미키')
+b.printData()
+
+reload(bookstore)
