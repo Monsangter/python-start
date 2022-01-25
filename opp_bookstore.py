103b872e2abc2bb2900894fb128fc51643e98e45
monsangter <sangdobae@icloud.com>
2022-01-25 22:53:51 +0900

    아아

--- opp_bookstore.py
+++ opp_bookstore.py
@@ -14,10 +14,15 @@ class Book:
         self.setData(title, price, author)
         print('책 객체를 새로 만들었어요~')
 
+    def __repr__(self):
+        return self.title
+
 
 b=Book()
 b.setData('누가 내 치즈를 먹었을까', '300원', '미키')
 b.printData()
 
 b2=Book()
-b2.setData('내가 먹었지롱, '200원', '미니')
+b2.setData('내가 먹었지롱', '200원', '미니')
+
+b3=Book('나두 좀 줘', '100원', '쥐벼룩')
